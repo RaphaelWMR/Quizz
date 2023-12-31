49604e5a7f6cac94d5a3ad9eee1bd2c5529c4ac6
Raphael Matos <matosramosraphael@gmail.com>
2023-12-31 12:18:51 -0500

    Creacion de modulos y componentes

---
+++ src/app/components/usuario/usuario-routing.module.ts
@@ -0,0 +1,13 @@
+//Un archivo de routing permite movernos
+//entre los componentes
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsuarioRoutingModule { }
